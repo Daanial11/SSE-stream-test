ed7ec1d04fc31845f95644c7a6bca1700adc6058
Daanial Ahmed <daaahmed1@WKMZT509C04E.global.publicisgroupe.net>
2023-09-08 15:51:38 +0100

    init

---
+++ src/main/java/dev/gokhana/sse/ServerSentEventsApplication.java
@@ -0,0 +1,15 @@
+package dev.gokhana.sse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ServerSentEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerSentEventsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/gokhana/sse/service/LoopPostEvent.java
@@ -0,0 +1,42 @@
+package dev.gokhana.sse.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+
+@Service
+public class LoopPostEvent{
+
+    
+    @Scheduled(fixedRate=1000)
+    public void postEvent() {
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost request = new HttpPost("http://localhost:8080/api/v1/live-scores");
+        String scores = "{\"homeTeam\":\"Arsenal\", \"awayTeam\":\"Tottenham\", \"homeScore\": 1, \"awayScore\": 1}";
+        request.addHeader("Content-Type", "application/json");
+        try {
+            request.setEntity(new StringEntity(scores));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpResponse response = httpclient.execute(request);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
