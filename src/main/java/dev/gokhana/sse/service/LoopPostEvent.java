0a6a56f562d58bbbd9391f54af5b3d031b6a2dd2
Daanial Ahmed <daaahmed1@WKMZT509C04E.global.publicisgroupe.net>
2023-09-20 15:12:54 +0100

    1 sec

--- src/main/java/dev/gokhana/sse/service/LoopPostEvent.java
+++ src/main/java/dev/gokhana/sse/service/LoopPostEvent.java
@@ -20,7 +20,7 @@ import org.apache.http.impl.client.HttpClients;
 public class LoopPostEvent{
 
     
-    @Scheduled(fixedRate=10000)
+    @Scheduled(fixedRate=1000)
     public void postEvent() {
         HttpClient httpclient = HttpClients.createDefault();
         HttpPost request = new HttpPost("http://localhost:8080/api/v1/live-scores");
